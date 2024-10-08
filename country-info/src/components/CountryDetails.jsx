4e3ad14d09e1dcc1e3cc499d02fc7ca01e834a4f
Ahsan <sajibnet@gmail.com>
2024-10-08 10:11:17 +0300

    Country info App added 2.18-2.20

--- country-info/src/components/CountryDetails.jsx
+++ country-info/src/components/CountryDetails.jsx
@@ -1,16 +1,41 @@
 // src/components/CountryDetails.jsx
+
+
 import React from 'react';
+import Weather from './Weather';
 
 const CountryDetails = ({ country }) => {
+  if (!country) return null;
+
+  const { name, capital, area, languages, flags } = country;
+
   return (
     <div>
-      <h2>{country.name.common}</h2>
-      <p><strong>Capital:</strong> {country.capital[0]}</p>
-      <p><strong>Area:</strong> {country.area} km²</p>
-      <p><strong>Languages:</strong> {Object.values(country.languages).join(', ')}</p>
-      <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="100" />
+      <h2>{name.common}</h2>
+      <img src={flags.png} alt={`Flag of ${name.common}`} />
+      <p>Capital: {capital[0]}</p>
+      <p>Area: {area} km²</p>
+      <p>Languages: {Object.values(languages).join(', ')}</p>
+      <Weather capital={capital[0]} />
     </div>
   );
 };
 
 export default CountryDetails;
+
+
+// import React from 'react';
+
+// const CountryDetails = ({ country }) => {
+//   return (
+//     <div>
+//       <h2>{country.name.common}</h2>
+//       <p><strong>Capital:</strong> {country.capital[0]}</p>
+//       <p><strong>Area:</strong> {country.area} km²</p>
+//       <p><strong>Languages:</strong> {Object.values(country.languages).join(', ')}</p>
+//       <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="100" />
+//     </div>
+//   );
+// };
+
+// export default CountryDetails;
